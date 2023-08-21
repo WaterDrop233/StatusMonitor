60ea4ef96955a889818e8df8588b177bed5c066f
WaterDrop233 <shuidi_111@163.com>
2023-08-21 17:07:16 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	mwindow.py

--- mwindow.py
+++ mwindow.py
@@ -26,6 +26,7 @@ class Ui_Form(object):
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
+
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "StatusMonitor"))

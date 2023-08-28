6dd4befa6af28995e46e8e189cf258ec50ccb337
WaterDrop233 <shuidi_111@163.com>
2023-08-28 09:26:24 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	mwindow.py

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import sys
-from function import status
+import psutil
+import GPUtil
 from mwindow import Ui_Form
-from PySide2.QtCore import QTimer
-from PySide2.QtGui import QIcon
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+def status(self):
+    def GetRAM():
+        RAMFree = str(round(psutil.virtual_memory().free / 1024 ** 3, 1))
+        RAMUsed = str(round(psutil.virtual_memory().used / 1024 ** 3, 1))
+        RAMStatus = "内存:已用:" + RAMUsed + "GB" + "\n" + "     未用:" + RAMFree + "GB"
+        return RAMStatus
+
+    def GetCPU():
+        CPUStatus = "CPU:已用:" + str(psutil.cpu_percent(interval=0.2))
+        return CPUStatus
+
+    def GetGPU():
+        if len(GPUtil.getGPUs()) != 0:
+            GPUStatus = "显卡:负载率:" + str(round(GPUtil.getGPUs()[0].load, 1)) + "\n" + "     显存使用率:" + str(
+                round(GPUtil.getGPUs()[0].memoryUtil, 1))
+        else:
+            GPUStatus = "未检测到显卡"
+        return GPUStatus
+
+    return str(GetCPU() + "\n" + GetRAM() + "\n" + GetGPU())
 
 
 class MyMainWindow(QMainWindow):
